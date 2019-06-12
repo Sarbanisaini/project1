6d1414ab036258e95749de4d716af177f2b55d4b
Sarbanisaini <srsarbani96@gmail.com>
2019-06-12 23:18:13 +0530

    sarbani

---
+++ src/sample.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+public class sample {
+	
+		public static void main(String[] args) throws Throwable  {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\SARBANI\\eclipse-workspace1\\Green Tech\\Screen\\Driver\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			driver.get("https://www.facebook.com/");
+			TakesScreenshot tk=(TakesScreenshot)driver;
+			File scr = tk.getScreenshotAs(OutputType.FILE);
+			File dec= new File("C:\\Users\\SARBANI\\eclipse-workspace1\\Green Tech\\Screen\\screenshot.png");
+			Files.copy(scr,dec);
+		}
+
+	}
+
+
